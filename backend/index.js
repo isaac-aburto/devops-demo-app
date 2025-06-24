d78572570395b2b58cf62bcadc2d40847ba3cef4
isaac-aburto <73562356+isaac-aburto@users.noreply.github.com>
2025-06-24 16:09:14 -0400

    cambios bakcend para validar su funcionamiento

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hola desde el backend!'));
-app.listen(3001, () => console.log('Backend corriendo en puerto 3001'));
+
+app.get('/api', (req, res) => {
+  res.send('Backend funcionando!');
+});
+app.listen(3000, () => console.log('Backend corriendo en puerto 3001'));
+
