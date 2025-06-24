3590bfa7ddf22dbafe82cf67c16bfda09bacd1ad
isaac-aburto <73562356+isaac-aburto@users.noreply.github.com>
2025-06-24 17:37:57 -0400

    cambios bakcend para validar su funcionamiento y deploy 2

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get(['/api', '/api/'], (req, res) => {
-  res.send('Backend funcionando!');
+  res.send('Backend funcionando!!');
 });
 app.listen(3000, () => console.log('Backend corriendo en puerto 3000'));
 
