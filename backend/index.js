81a898d4566df58f9a398dd78a87128dce9c78e1
isaac-aburto <73562356+isaac-aburto@users.noreply.github.com>
2025-06-25 10:47:52 -0400

    cambios frontend para validar pablo

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get(['/api', '/api/'], (req, res) => {
-  res.send('Backend funcionando - Alejandro!!');
+  res.send('Backend funcionando - Pablo!!');
 });
 app.listen(3000, () => console.log('Backend corriendo en puerto 3000'));
 
